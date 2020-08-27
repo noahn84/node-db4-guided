96ecab70312b6dcdbe0dd5990092fe658cb90d62
Jason Maurer <jason@maur.co>
2020-08-26 19:39:38 -0600

    Code from guided lecture

--- models/zoo.js
+++ models/zoo.js
@@ -10,7 +10,23 @@ function findById(id) {
 		.first()
 }
 
+function findAnimals(zooID) {
+	return db("zoos_animals as za")
+		.join("zoos as z", "z.id", "za.zoo_id")
+		.join("animals as a", "a.id", "za.animal_id")
+		.join("species as s", "s.id", "a.species_id")
+		.where("z.id", zooID)
+		.select(
+			"a.id",
+			"a.name",
+			"s.name as species_name",
+			"za.from_date",
+			"za.to_date",	
+		)
+}
+
 module.exports = {
 	find,
 	findById,
+	findAnimals,
 }
